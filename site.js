e66e8f215e1db0d17a6cd060637ace0e9d7f54a9
Ibrahium-Lashari <524Ilashari@frhsd.com>
2024-05-07 12:55:33 -0400

    step 3 uncomplete

--- site.js
+++ site.js
@@ -33,6 +33,9 @@ const vue_app = Vue.createApp({
         return {
             // This holds your movies.json data.
             movies: [],
+            title: "IMDB + Ibrahims Top 8 Movies",
+            owner: "Ibrahim",
+            github: 
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
          
       }
