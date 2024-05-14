af80b1f177e2bb409fde54e9aa64de2852989f91
Ibrahium-Lashari <524Ilashari@frhsd.com>
2024-05-14 14:51:04 -0400

    get Month Text

--- site.js
+++ site.js
@@ -20,46 +20,79 @@
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
 */
 
-
 const vue_app = Vue.createApp({
-      // This automatically imports your movies.json file and puts it into
-      //   the variable: movies
-      created () {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
-      },
-      data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-            title: 'IMDB + Ibrahims Top 8 Movies',
-            owner: 'Ibrahim',
-            github: 'https://github.com/Ibrahim-Lashari?tab=repositories',
+  // This automatically imports your movies.json file and puts it into
+  //   the variable: movies
+  created() {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((json) => {
+        this.movies = json;
+      });
+  },
+  data() {
+    return {
+      // This holds your movies.json data.
+      movies: [],
+      /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+      title: "IMDB + Ibrahims Top 8 Movies",
+      owner: "Ibrahim",
+      github: "https://github.com/Ibrahim-Lashari?tab=repositories",
+    };
+  },
+  methods: {
+    /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+    getMonthText(dateArray) {
+      var year = dateArray[0];
+      var day = dateArray[2];
+      let month;
+      switch (dateArray[1]) {
+        case 1:
+          month = "January";
+          break;
+        case 2:
+          month = "February";
+          break;
+        case 3:
+          month = "March";
+          break;
+        case 4:
+          month = "April";
+          break;
+        case 5:
+          month = "May";
+          break;
+        case 6:
+          month = "June";
+          break;
+        case 7:
+          month = "July";
+          break;
+        case 8:
+          month = "August";
+          break;
+        case 9:
+          month = "September";
+          break;
+        case 10:
+          month = "October";
+          break;
+        case 11:
+          month = "November";
+          break;
+        case 12:
+          month = "December";
+          break;
+        default:
+        // code block
       }
+      return month + " " + day + ", " + year;
     },
-      methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-            getMonthText(dateArray) {
-                  
-            },
-            like(index) {
+    like(index) {},
+    dislike(index) {},
+    posterClick(index) {},
+    timeText(minutes) {},
+  },
+});
 
-            },
-            dislike(index) {
-
-            },
-            like(index) {
-
-            },
-            posterClick(index) {
-
-            },
-            timeText(minutes) {
-
-            },
-      }
-})
-
-vue_app.mount("#vue_app")
+vue_app.mount("#vue_app");
